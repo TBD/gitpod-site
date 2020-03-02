309d6b3d70f2b934de6a37b0c4fc723e31263ba3
TBD <tbd_fr@yahoo.fr>
2020-03-02 08:12:34 +0000

    changes

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
 #/usr/bin/sh
 npm install
 mkdir -p /workspace/data 
-mongod --dbpath /workspace/data
+mongod --dbpath /workspace/data && mongoimport --db temp --collection temp < bin/initial_data.json
